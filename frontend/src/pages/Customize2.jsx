502e467c8588781dba29a61e073c524d0b1224bd
RitikaSahu1310 <ritikasahu2012@gmail.com>
2026-04-03 22:06:32 +0530

    Update Customize2.jsx

--- frontend/src/pages/Customize2.jsx
+++ frontend/src/pages/Customize2.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import UserContext, { userdataContext } from '../context/userContext'
+import UserContext, { userdataContext } from '../context/UserContext'
 import axios from 'axios'
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { useNavigate } from 'react-router-dom';
