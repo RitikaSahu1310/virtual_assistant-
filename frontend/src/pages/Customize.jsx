86aafdff53a5ef93ec033059a1d087d7c3127b6c
RitikaSahu1310 <ritikasahu2012@gmail.com>
2026-04-03 22:06:13 +0530

    Update Customize.jsx

--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -8,7 +8,7 @@ import image5 from "../assets/image5.png"
 import image6 from "../assets/image6.jpeg"
 import image7 from "../assets/image7.jpeg"
 import { RiImageAddFill } from "react-icons/ri";
-import { userdataContext } from '../context/userContext'
+import { userdataContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
 import { IoMdArrowRoundBack } from "react-icons/io";
 
